6a8d07705dcf2aef5e4dc92cbd85e833da2345af
saiharshitha123 <52479103+saiharshitha123@users.noreply.github.com>
2019-07-04 11:23:33 +0530

    Add files via upload

---
+++ romantointeger.py
@@ -0,0 +1,5 @@
+y1=input()
+y2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+    if(y1==y2[i]):
+        print(i)
