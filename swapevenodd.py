a47dd118cf1b6bce8e4095846200c58f931af374
saiharshitha123 <52479103+saiharshitha123@users.noreply.github.com>
2019-07-04 12:01:07 +0530

    Add files via upload

---
+++ swapevenodd.py
@@ -0,0 +1,8 @@
+def swapBits(x):
+    even_bits = x & 0xAAAAAAAA
+    odd_bits = x & 0x55555555
+    even_bits >>=1
+    odd_bits <<=1
+    return (even_bits | odd_bits)
+    x=23
+    print(swapBits(x))
